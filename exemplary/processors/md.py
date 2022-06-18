10f1b86a08d35885393d27ae3bd01e81e5844766
Jajo <shacham6@gmail.com>
2022-06-18 11:23:13 +0300

    Great Progress

--- exemplary/processors/md.py
+++ exemplary/processors/md.py
@@ -1,3 +1,5 @@
+import re
+
 from .base import Config, ProcessorBase, Segment
 
 
@@ -7,13 +9,37 @@ class MarkdownProcessor(ProcessorBase):
     """
 
     def process(self, segment: Segment, config: Config) -> str:
-        return "\n".join(
-            _remove_prefix(line, segment.comment_pat)
-            for line in segment.document.splitlines()
-        )
+        lines = []
+        min_whitespace_buf = float("inf")
+        for line in segment.document.splitlines():
+            line = _remove_prefix(line, segment.comment_pat)
+            line_whitespace_count = whitespace_count(line)
+            if min_whitespace_buf > line_whitespace_count:
+                min_whitespace_buf = line_whitespace_count
+            lines.append(line)
+
+        if min_whitespace_buf < float("inf"):
+            min_whitespace_buf = int(min_whitespace_buf)
+            lines = [
+                _remove_prefix(line, " " * int(min_whitespace_buf))
+                for line in lines
+            ]
+
+        return "\n".join(lines)
 
 
 def _remove_prefix(text: str, prefix: str) -> str:
     if text.startswith(prefix):
         return text[len(prefix):]  # fmt: skip
     return text
+
+
+_WHITESPACES_PAT = re.compile(r"^( +)[^ ]?")
+
+
+def whitespace_count(text: str) -> int:
+    match = _WHITESPACES_PAT.match(text)
+    if not match:
+        return 0
+
+    return len(match.group(1))
--- tests/processors_tests/test_markdown.py
+++ tests/processors_tests/test_markdown.py
@@ -1,25 +1,62 @@
-from exemplary.processors.md import MarkdownProcessor, Segment, Config
+from pytest_subtests import SubTests
+
+from exemplary.processors.md import (
+    Config,
+    MarkdownProcessor,
+    Segment,
+    whitespace_count,
+)
 
 
-def test_comment_stripping():
+def test_comment_stripping(subtests: SubTests):
     processor = MarkdownProcessor()
-    result = processor.process(
-        Segment(
-            "md",
-            {},
-            _text(
-                "# Line 1",
-                "# Line 2",
+    with subtests.test("Basic"):
+        result = processor.process(
+            Segment(
+                "md",
+                {},
+                _text(
+                    "# Line 1",
+                    "# Line 2",
+                ),
+                "#",
             ),
-            "#",
-        ),
-        Config(inputs=[]),
-    )
-    assert result == _text(
-        "Line 1",
-        "Line 2",
-    )
+            Config(inputs=[]),
+        )
+        assert result == _text(
+            "Line 1",
+            "Line 2",
+        )
+
+    with subtests.test("Strips only minimal whitespace buffers"):
+        result = processor.process(
+            Segment(
+                "md",
+                {},
+                _text(
+                    "# Only 1 Whitespace",
+                    "#  2 Whitespaces",
+                ),
+                "#",
+            ),
+            Config(inputs=[]),
+        )
+        assert result == _text(
+            "Only 1 Whitespace",
+            " 2 Whitespaces",  # Notice the extra whitespace in the beginning.
+        )
 
 
 def _text(*lines: str) -> str:
     return "\n".join(lines)
+
+
+def test_whitespace_count(subtests: SubTests):
+    with subtests.test("Simple"):
+        assert whitespace_count("") == 0
+        assert whitespace_count(" ") == 1
+        assert whitespace_count(" Something") == 1
+
+    with subtests.test("Counts only starters"):
+        assert whitespace_count(" 1  2   3") == 1
+        assert whitespace_count("  2   3") == 2
